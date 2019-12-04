539261a0c101aa4da3e119dc115c3d764d69646d
Varun Bansal <varun330@gmail.com>
2019-12-05 05:20:14 +0530

    fixes 2

--- eod/mlapp/functions.py
+++ eod/mlapp/functions.py
@@ -1,11 +1,12 @@
 import random
 import pandas as pd
 
+dangerous = ["Isopropanol"]
 def predict_class(dataframe):
     smell_classes = ["Air", "Lime", "Vodka", "Beer", "Vinegar", "Wine", "Acetone", "Ethanol", "Isopropanol"]
     smell_class = random.choice(smell_classes)
 
-    if smell_class == "Isopropanol":
+    if smell_class in dangerous:
         return smell_class, "Dangerous"
     else:
         return smell_class, "Normal"
