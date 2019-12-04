a0f48dbe063c039bf6ff67e79f40afa8ffe4aada
Varun Bansal <varun330@gmail.com>
2019-12-05 00:35:01 +0530

    fixes 4

--- Raspberry Pi Scripts (with mlapp)/data_collection.py
+++ Raspberry Pi Scripts (with mlapp)/data_collection.py
@@ -26,35 +26,41 @@ ser.baudrate=9600
 
 gp.setmode(gp.BOARD)
 smell_class = input("Enter Smell Class: ")
-rows = list()
 x = 1
-for i in range(1,101):
-    if x>60:
-        x = 1
-    try:
-        read_ser=ser.readline()
-        temp_string = read_ser.decode('utf-8')
-        temp_data = eval(temp_string[:-2])
-        # temp_data = random_generator()
-        temp_data["time"] = x
-        data = {
-        "time":temp_data["time"],
-        "temp":temp_data["temp"],
-        "pres":temp_data["pres"],
-        "co":temp_data["co"],
-        "lpg": temp_data["lpg"],
-        "smoke": temp_data["smoke"]
-        }
-        print(data)
-        rows.append(data)
-        x += 1
-        time.sleep(1)
-    except:
-        print("Try Again")
+y = 1
 now = int(datetime.timestamp(datetime.now()))
-keys = rows[0].keys()
-with open(str(smell_class)+'_'+str(now)+'.csv', 'w') as output_file:
-    dict_writer = csv.DictWriter(output_file, keys)
-    dict_writer.writeheader()
-    dict_writer.writerows(rows)
-    print(str(smell_class)+'_'+str(now)+'.csv')
+print(str(smell_class)+'_'+str(now)+'.csv')
+keys = ["time","temp","pres", "co", "lpg", "smoke"]
+try:
+    with open(str(smell_class)+'_'+str(now)+'.csv', 'w') as output_file:
+        dict_writer = csv.DictWriter(output_file, fieldnames = keys)
+        dict_writer.writeheader()
+        for i in range(1,5401):
+            if x>60:
+                x = 1
+            try:
+                read_ser=ser.readline()
+                temp_string = read_ser.decode('utf-8')
+                temp_data = eval(temp_string[:-2])
+                # temp_data = random_generator()
+                temp_data["time"] = x
+                data = {
+                "time":temp_data["time"],
+                "temp":temp_data["temp"],
+                "pres":temp_data["pres"],
+                "co":temp_data["co"],
+                "lpg": temp_data["lpg"],
+                "smoke": temp_data["smoke"]
+                }
+                print(y,data)
+                dict_writer.writerow(data)
+                x += 1
+                y += 1
+                time.sleep(1)
+            except KeyboardInterrupt:
+                sys.exit(0)
+            except:
+                print("Try Again")
+except IOError:
+    print("I/O Error")
+print('Data Collection Over')
